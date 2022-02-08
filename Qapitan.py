93438b1311eb63b216bbed674747870d0372ce15
Sergio Gago <sergiogh@gmail.com>
2022-02-08 15:13:19 +0100

    added maxcut

--- Qapitan.py
+++ Qapitan.py
@@ -10,28 +10,42 @@ class Qapitan:
 
     def login(self):
         response = requests.post(self.QAPITAN_PUBLIC_API + '/login', json=self.PAYLOAD_USER)
-        response_json = response.json()
-
-        print(response_json)
-        
-        access_token = response_json['access_token']
-        header = {'Authorization': 'Bearer ' + access_token}
-        return header
+        if(response.status_code == 200):
+            response_json = response.json()
+            access_token = response_json['access_token']
+            header = {'Authorization': 'Bearer ' + access_token}
+            return header
+        else:
+            return response.status_code        
 
     def execute(self, header, problem, payload):
         response = requests.post(self.QAPITAN_PUBLIC_API + '/problem/' + problem, json=payload, headers=header)
-        response_json = response.json()
-        return response_json
+        if(response.status_code == 200):
+            response_json = response.json()
+            return response_json
+        else:
+            return response.status_code       
 
     def get_result(self, header, job_name):
         response = requests.get(self.QAPITAN_PUBLIC_API + "/job/" + job_name, headers=header)
-        return response.json()
+        if(response.status_code == 200):
+            response_json = response.json()
+            return response_json
+        else:
+            return response.status_code   
+
 
     def get_results(self, header):
         response = requests.get(self.QAPITAN_PUBLIC_API + "/results", headers=header)
-        return response.json()
+        if(response.status_code == 200):
+            response_json = response.json()
+            return response_json
+        else:
+            return response.status_code
 
     def get_best_result(self, header, job_name):
         result = self.get_result(header, job_name)
-        for i in result['job'][job_name]['executions']:
-            return result['job'][job_name]['executions'][i]['result']
+        if('job' in result):
+            for i in result['job'][job_name]['executions']:
+                return result['job'][job_name]['executions'][i]['result']
+        return "500"
